a35fa1bbd67404db208e1df23d03bdf9915631e2
2gn <101851090+2gn@users.noreply.github.com>
2025-01-10 15:36:40 +0900

    I hate notion

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'SHAC-Blog-1767ef8a65ff806289dec0a57f1794c3',
+  rootNotionPageId: '1775b6cbb18080dc9f00fb5cc58539a2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
